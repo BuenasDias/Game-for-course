ab420eab13d76c34dca3cdf6a633b61cfc0e1000
BuenasDias <tmaksim1989@gmail.com>
2021-10-03 13:20:39 +0300

    create lesson_1

---
+++ app/src/main/java/com/max/interview/gameforcourse/GameFragment.java
@@ -0,0 +1,49 @@
+package com.max.interview.gameforcourse;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.max.interview.gameforcourse.databinding.FragmentGameBinding;
+import com.max.interview.gameforcourse.databinding.FragmentMainBinding;
+
+import java.util.Objects;
+
+public class GameFragment extends Fragment {
+
+    private FragmentGameBinding binding;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentGameBinding.inflate(LayoutInflater.from(requireParentFragment().getContext()));
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        navHostFragment = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = Objects.requireNonNull(navHostFragment).getNavController();
+
+        binding.goMainFragment.setOnClickListener(v ->
+                navController.navigate(R.id.action_gameFragment_to_mainFragment,
+                        null,
+                        new NavOptions.Builder()
+                                .setEnterAnim(R.anim.anim_show)
+                                .setExitAnim(R.anim.anim_hide)
+                                .build())
+        );
+    }
+}
--- app/src/main/java/com/max/interview/gameforcourse/MainActivity.java
+++ app/src/main/java/com/max/interview/gameforcourse/MainActivity.java
@@ -1,14 +1,24 @@
 package com.max.interview.gameforcourse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.max.interview.gameforcourse.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
+
     }
 }
